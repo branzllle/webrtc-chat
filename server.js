704dae3c9b3bf3c362b247c1f7509a54d60ff149
HASSAN A <87488089+branzllle@users.noreply.github.com>
2025-06-17 09:55:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,6 @@ io.on('connection', socket => {
   });
 
   socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id);
     if (waitingUser === socket) waitingUser = null;
     if (socket.partner) {
       socket.partner.emit('partner-disconnected');
